437d7955ae43066fee3f89ddba9497f490ff257a
xushijun5210 <2273525664@qq.com>
2020-05-25 08:58:10 +0800

    3
    添加home模块

---
+++ pages/userhome/userhome.vue
@@ -0,0 +1,27 @@
+<template>
+	<view>
+		<view class="top-bar">
+			<view class="top-bar-left" @tap="backOne">
+				<image src="../../static/img/fanhui.png" class="back-img top-bar-left-img" mode=""></image>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		},
+		methods:{
+			
+		}
+		
+	}
+</script>
+
+<style lang="scss">
+@import url("userhome.css");
+</style>
